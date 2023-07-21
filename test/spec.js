2276335f71da01051e54a57cffac34e71ec0c380
Alan Koh <alanskoh@gmail.com>
2023-07-20 19:28:12 -0500

    added sync n seed

--- test/spec.js
+++ test/spec.js
@@ -9,3 +9,14 @@ const { expect } = require("chai");
 it("shows that true is true", () => {
   expect(true).to.be.equal(true);
 });
+
+describe("/api", () => {
+   //run the syncAndSeed before each block
+   beforeEach(async () => {
+     await syncAndSeed();
+   });
+
+
+
+
+});
